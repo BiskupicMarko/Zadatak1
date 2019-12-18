230d82c2734701e5dbd9ad49b61ff61d3db8523c
Marko Biskupic <biskupuicmarko4@gmail.com>
2019-12-18 11:17:48 +0100

    First commit

---
+++ predlozak/izbornik.php
@@ -0,0 +1,14 @@
+<!-- Početak izbornik -->
+<div class="title-bar" data-responsive-toggle="example-animated-menu" data-hide-for="medium">
+<button class="menu-icon" type="button" data-toggle></button>
+<div class="title-bar-title"><?=$nazivAPP?></div>
+</div>
+<div class="top-bar" id="example-animated-menu" data-animate="hinge-in-from-top spin-out">
+<div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu>
+    <li class="menu-text show-for-large"><?php echo $nazivAPP ?></li>
+    <li><a href="index.php">Početna</a></li>
+    </ul>
+</div>
+</div>
+<!-- Kraj izbornik -->
